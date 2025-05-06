972681b56c71cd6c525118da809719a096b3d206
unknown <irgiansyahben86ifs@gmail.com>
2025-05-06 15:17:56 +0700

    testing kedua

---
+++ backend/kematelyu/src/test/java/com/kematelyu/kematelyu/KematelyuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kematelyu.kematelyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KematelyuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
