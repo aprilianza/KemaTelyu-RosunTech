7fa131a7fe10e3b9284ead504e09b78ad036baec
Athila Ramdani Saputra <68465902+athilaramdani@users.noreply.github.com>
2025-05-09 15:14:41 +0700

    add auth login

--- backend/kematelyu/src/main/java/com/kematelyu/kematelyu/model/Staff.java
+++ backend/kematelyu/src/main/java/com/kematelyu/kematelyu/model/Staff.java
@@ -1,25 +1,31 @@
 package com.kematelyu.kematelyu.model;
-import java.util.List;
 
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 @Entity
 public class Staff extends User {
+
     private String divisi;
+
     public void createEvent(Event event) {
         if (event != null) {
             event.setCreatedBy(this);
         }
     }
+
     public List<Mahasiswa> viewParticipant(Event event) {
         return event.getParticipants();
     }
+
     public String getDivisi() {
         return divisi;
     }
+
     public void setDivisi(String divisi) {
         if (divisi == null || divisi.trim().isEmpty()) {
             throw new IllegalArgumentException("Divisi tidak boleh kosong.");
