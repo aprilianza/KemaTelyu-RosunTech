28b19b14588a6744cbf28af9607f7c67a52fc0de
aprilianza <138241892+aprilianza@users.noreply.github.com>
2025-05-11 14:21:17 +0700

    styling login page

--- frontend/rosuntech-frontend/src/main.js
+++ frontend/rosuntech-frontend/src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
+import Swal from 'sweetalert2'
+import colors from './utils/colors'
+// Buat aplikasi Vue dan simpan ke dalam variabel app
+const app = createApp(App)
+app.config.globalProperties.$colors = colors
 
-createApp(App).use(router).mount('#app')
+// Tambahkan SweetAlert2 sebagai properti global
+app.config.globalProperties.$swal = Swal
+
+// Gunakan router dan mount aplikasi
+app.use(router).mount('#app')
